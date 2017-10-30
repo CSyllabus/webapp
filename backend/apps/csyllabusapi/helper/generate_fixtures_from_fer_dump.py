b507f1a8891877ab64e4f4a5e939fe2654846083
Guber <emanuel.guberovic@fer.hr>
2017-10-30 18:17:00 +0100

    added a parser scrypt for course data to fixtures

---
+++ backend/apps/csyllabusapi/helper/generate_fixtures_from_fer_dump.py
@@ -0,0 +1,106 @@
+import json
+
+fer_course_json = open("fer_courses.json")
+fer_fixtures_json = open("../fixtures/fer_fixtures_json.json", "w")
+fer_courses = json.load(fer_course_json)
+
+fixtures = []
+programs_fixtures = []
+programs = []
+
+fixtures.append({
+    "model": "csyllabusapi.country",
+    "pk": 1,
+    "fields": {
+      "name": "Croatia",
+      "created": "2017-10-30T15:20:51.049Z",
+      "modified": "2017-10-30T15:20:52.235Z"
+    }
+  }
+)
+
+fixtures.append(
+  {
+    "model": "csyllabusapi.university",
+    "pk": 1,
+    "fields": {
+      "name": "University of Zagreb",
+      "created": "2017-10-30T15:05:19.541Z",
+      "modified": "2017-10-30T15:05:20.945Z",
+      "country": 1
+    }
+  }
+)
+
+fixtures.append(
+  {
+    "model": "csyllabusapi.faculty",
+    "pk": 1,
+    "fields": {
+      "name": "Faculty of electrical engineering and computing",
+      "city": "Zagreb",
+      "created": "2017-10-30T15:05:45.387Z",
+      "modified": "2017-10-30T15:05:52.444Z",
+      "university": 1
+    }
+  }
+)
+
+#appending programs fixtures
+program_id = 1
+for course in fer_courses:
+    if course['profile'] not in programs:
+        programs.append(course['profile'])
+        study_level = ""
+        if course['studylevel'] == 3:
+            study_level = "undergraduate"
+        elif course['studylevel'] == 4:
+            study_level = "graduate"
+        elif course['studylevel'] == "3, 4":
+            study_level = "undergraduate and graduate"
+        program = {
+            "model": "csyllabusapi.program",
+            "pk": program_id,
+            "fields": {
+                "name" : course['profile'],
+                "study_level": study_level,
+                "created": "2017-10-30T15:07:40.122Z",
+                "modified": "2017-10-30T15:07:41.673Z",
+                "faculty": 1
+            }
+        }
+        fixtures.append(program)
+        programs_fixtures.append(program)
+        program_id = program_id + 1
+
+
+
+#appending courses fixtures
+course_id = 1
+for course in fer_courses:
+    program = -1
+    for program in programs_fixtures:
+        if program["fields"]["name"] == course['profile']:
+            program = int(program["pk"])
+            break
+
+    fixtures.append(
+        {
+            "model": "csyllabusapi.course",
+            "pk": course_id,
+            "fields": {
+                "name" : course['coursename'],
+                "description": course['coursedescription'],
+                "ects": course['ects'],
+                "english_level": course['englevel'],
+                "semester": course['semester'],
+                "winsum": course['winsumm'],
+                "created": "2017-10-30T15:07:40.122Z",
+                "modified": "2017-10-30T15:07:41.673Z",
+                "program": program
+            }
+        }
+    )
+    course_id = course_id + 1
+
+json.dump(fixtures,fer_fixtures_json)
--- backend/apps/csyllabusapi/models/program.py
+++ backend/apps/csyllabusapi/models/program.py
@@ -3,7 +3,7 @@ from django.db import models
 from .faculty import Faculty
 
 class Program(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=500)
     study_level = models.CharField(max_length=255)
     created = models.DateTimeField(editable=False)
     modified = models.DateTimeField()
