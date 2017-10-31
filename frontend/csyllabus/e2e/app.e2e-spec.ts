e8277ad44807e11c03e2d7189b57d5576803a98a
filt27 <filt.27@gmail.com>
2017-11-01 00:36:04 +0100

    Angular 4 + Material

---
+++ frontend/csyllabus/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CsyllabusPage } from './app.po';
+
+describe('csyllabus App', () => {
+  let page: CsyllabusPage;
+
+  beforeEach(() => {
+    page = new CsyllabusPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
