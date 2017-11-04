7267de54de0d798827ce3b8dfa9b2d776726de6d
Adrien Roques <adrien.roques.31@outlook.fr>
2017-11-04 13:46:18 +0100

    Modify comments

--- frontend/csyllabus/src/app/angular-material/explorer/explorer.result.ts
+++ frontend/csyllabus/src/app/angular-material/explorer/explorer.result.ts
@@ -4,6 +4,13 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Result } from './result'
 
+/** Constants used to fill up our data base. */ //TEST DATA 
+const SCORE = ['10', '20', '60', '55', '40', '80'];
+const COUNTRY = ['FRANCE', 'US', 'ITALY', 'CROATIA', 'FRANCE', 'UK'];
+const UNIVERSITY = ['IUT', 'POLIMI', 'STANFORD', 'ZAGREB', 'PARIS1', 'OXFORD'];
+const PROFESSOR = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack'];
+const SYLLABUS = ['1', '2', '3', '4', '5', '6'];
+
 /* -*- coding: utf - 8 -*-
 # @Author: Adrien Roques
 # @title: ExplorerResult
@@ -11,36 +18,27 @@ import { Result } from './result'
 # @Last Modified by: Adrien Roques
 # @Last Modified time: 2017 - 11 - 03
 # @Description: Manage graphics datas*/
-
-/** Constants used to fill up our data base. */
-const SCORE = ['10', '20', '60', '55', '40', '80'];
-const COUNTRY = ['FRANCE', 'US', 'ITALY', 'CROATIA', 'FRANCE', 'UK'];
-const UNIVERSITY = ['IUT', 'POLIMI', 'STANFORD', 'ZAGREB', 'PARIS1', 'OXFORD'];
-const PROFESSOR = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack'];
-const SYLLABUS = ['1', '2', '3', '4', '5', '6'];
-
-
-/** An example database that the data source uses to retrieve data for the table. */
 export class ResultExplorer {
     /** Stream that emits whenever the data has been modified. */
     dataChange: BehaviorSubject<Result[]> = new BehaviorSubject<Result[]>([]);
     get data(): Result[] { return this.dataChange.value; }
 
     constructor() {
-        // Fill up the database with 100 users.
-        for (let i = 0; i < 5; i++) { this.addUser(); }
+        // Fill up the database with 5 rows test function, not connencted yet
+        for (let i = 0; i < 5; i++) { this.addRow(); }
     }
 
-    /** Adds a new user to the database. */
-    addUser() {
+    /** Adds a new row to the database. */
+    addRow() {
         const copiedData = this.data.slice();
-        copiedData.push(this.createNewUser());
+        copiedData.push(this.createNewRow());
         this.dataChange.next(copiedData);
     }
 
-    /** Builds and returns a new User. */
-    private createNewUser() {
+    /** Builds and returns a new row. */
+    private createNewRow() {
 
+        //TODO DELETE WHEN CONNECTED
         const score = SCORE[Math.round(Math.random() * (SCORE.length - 1))];
         const country = COUNTRY[Math.round(Math.random() * (COUNTRY.length - 1))];
         const university = UNIVERSITY[Math.round(Math.random() * (UNIVERSITY.length - 1))];
