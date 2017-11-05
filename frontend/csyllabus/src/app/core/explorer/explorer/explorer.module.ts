9fdceb2af1c1a6c34adebdd0949feb68e3e903a2
Sebastian Mayoral <obandomayoral@gmail.com>
2017-11-05 15:37:24 +0100

    Organized

--- frontend/csyllabus/src/app/core/explorer/explorer/explorer.module.ts
+++ frontend/csyllabus/src/app/core/explorer/explorer/explorer.module.ts
@@ -1,7 +1,7 @@
 import { ExplorerComponent } from './explorer.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from './../../angular-material/angular-material.module';
+import { AngularMaterialModule } from './../../../angular-material/angular-material.module';
 
 @NgModule({
 
