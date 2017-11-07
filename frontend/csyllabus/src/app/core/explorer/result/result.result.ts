e6e650a0003678b67efe2c7aeb54eb350e3aea75
Adrien Roques <adrien.roques.31@outlook.fr>
2017-11-07 23:20:51 +0100

    Add Grid to Card

--- frontend/csyllabus/src/app/core/explorer/result/result.result.ts
+++ frontend/csyllabus/src/app/core/explorer/result/result.result.ts
@@ -6,11 +6,20 @@ import { Result } from './result'
 
 /** Constants used to fill up our data base. */ //TEST DATA 
 const SCORE = ['10', '20', '60', '55', '40', '80'];
+const BLAZON = ['https://source.unsplash.com/user/erondu/200x200',
+    'https://source.unsplash.com/user/erondu/200x200',
+    'https://source.unsplash.com/user/erondu/200x200',
+    'https://source.unsplash.com/user/erondu/200x200',
+    'https://source.unsplash.com/user/erondu/200x200'];
 const COUNTRY = ['FRANCE', 'US', 'ITALY', 'CROATIA', 'FRANCE', 'UK'];
 const UNIVERSITY = ['IUT', 'POLIMI', 'STANFORD', 'ZAGREB', 'PARIS1', 'OXFORD'];
 const PROFESSOR = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack'];
 const SYLLABUS = ['Machine Learning', 'Computer Science', 'Embbeded System', 'Software Control', ' Computer Performance ', 'DataBase'];
-
+const DESCRIPTION = ['Computer science is the study of the theory, experimentation, and engineering that form the basis for the design and use of computers. It is the scientific and practical approach to computation and its applications and the systematic study of the feasibility, structure, expression, and mechanization of the methodical procedures (or algorithms) that underlie the acquisition, representation, processing, storage, communication of, and access to information.',
+    ' An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[1]Its fields can be divided into a variety of theoretical and practical disciplines.Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems)',
+    ' An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[1]Its fields can be divided into a variety of theoretical and practical disciplines.Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems)',
+    ' An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[1]Its fields can be divided into a variety of theoretical and practical disciplines.Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems)',
+    ' An alternate, more succinct definition of computer science is the study of automating algorithmic processes that scale. A computer scientist specializes in the theory of computation and the design of computational systems.[1]Its fields can be divided into a variety of theoretical and practical disciplines.Some fields, such as computational complexity theory (which explores the fundamental properties of computational and intractable problems)'];
 /* -*- coding: utf - 8 -*-
 # @Author: Adrien Roques
 # @title: ExplorerResult
@@ -44,13 +53,17 @@ export class ResultExplorer {
         const university = UNIVERSITY[Math.round(Math.random() * (UNIVERSITY.length - 1))];
         const professor = PROFESSOR[Math.round(Math.random() * (PROFESSOR.length - 1))];
         const syllabus = SYLLABUS[Math.round(Math.random() * (SYLLABUS.length - 1))];
+        const description = DESCRIPTION[Math.round(Math.random() * (DESCRIPTION.length - 1))];
+        const blazon = BLAZON[Math.round(Math.random() * (BLAZON.length - 1))];
 
         return {
             score: score,
             country: country,
             university: university,
             professor: professor,
-            syllabus: syllabus
+            syllabus: syllabus,
+            description: description,
+            blazon: blazon
         };
     }
 }
