ddf7ff255e3933ee95a641fcfbe370a7e0552dec
Guber <emanuel.guberovic@fer.hr>
2017-11-08 23:53:03 +0100

    fixed autocomple value bug

--- frontend/csyllabus/src/app/core/explorer/search/explorer.component.ts
+++ frontend/csyllabus/src/app/core/explorer/search/explorer.component.ts
@@ -1,10 +1,10 @@
-import { CoursesService } from './../../../services/courses.service';
-import { AngularMaterialModule } from './../../../angular-material/angular-material.module';
-import { Component, OnInit} from '@angular/core';
+import {CoursesService} from './../../../services/courses.service';
+import {AngularMaterialModule} from './../../../angular-material/angular-material.module';
+import {Component, OnInit} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FormControl} from '@angular/forms';
 import {FormControlDirective} from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
 
@@ -15,39 +15,42 @@ import 'rxjs/add/operator/startWith';
 })
 
 /* -*- coding: utf - 8 -*-
-# @Author: Sebastian
-# @title: TableFilteringExplorer
-# @Date: 2017 - 11 - 03
-# @Last Modified by: Adrien Roques
-# @Last Modified time: 2017 - 11 - 05
-# @Description: */
+ # @Author: Sebastian
+ # @title: TableFilteringExplorer
+ # @Date: 2017 - 11 - 03
+ # @Last Modified by: Adrien Roques
+ # @Last Modified time: 2017 - 11 - 05
+ # @Description: */
 export class ExplorerComponent implements OnInit {
+  coursesAvailable: any[];
+  controlCourses: FormControl = new FormControl();
 
-    constructor(
-        private coursesService:CoursesService,
-    ) {    
-    }
+  levels = [
+    {value: 'A1', viewValue: 'A1'},
+    {value: 'A2', viewValue: 'A2'},
+    {value: 'B1', viewValue: 'B1'},
+    {value: 'B2', viewValue: 'B2'},
+    {value: 'C1', viewValue: 'C1'},
+    {value: 'C2', viewValue: 'C2'}
+  ];
 
-    filteredOptions: Observable<string[]>;
-    
-    ngOnInit() { 
-        
-        console.log(this.coursesavailable);
-        console.log(this.Array);
-    }
-    levels = [
-        { value: 'A1', viewValue: 'A1' },
-        { value: 'A2', viewValue: 'A2' },
-        { value: 'B1', viewValue: 'B1' },
-        { value: 'B2', viewValue: 'B2' },
-        { value: 'C1', viewValue: 'C1' },
-        { value: 'C2', viewValue: 'C2' }
-    ];
-    semesters = [
-        { value: 1, viewValue: 1 },
-        { value: 2, viewValue: 2 }
-    ];
-    Array =["hola","adios"]
-   coursesavailable=this.coursesService.getAllCourses().subscribe(courses=>this.coursesavailable=courses); 
-    controlcourses: FormControl = new FormControl();
+  semesters = [
+    {value: 1, viewValue: 1},
+    {value: 2, viewValue: 2}
+  ];
+
+  constructor(private coursesService: CoursesService, ) {
+  }
+
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+
+
+  //Array = ["hola", "adios"]
+    //console.log(this.coursesAvailable);
+    //console.log(this.Array);
+    this.coursesService.getAllCourses().subscribe(courses => this.coursesAvailable = courses);
+  }
+
 }
--- frontend/csyllabus/src/app/core/explorer/search/explorer.module.ts
+++ frontend/csyllabus/src/app/core/explorer/search/explorer.module.ts
@@ -1,23 +1,24 @@
-import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
-import { ExplorerComponent } from './explorer.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from './../../../angular-material/angular-material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ExplorerComponent} from './explorer.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AngularMaterialModule} from './../../../angular-material/angular-material.module';
 
 @NgModule({
 
-    declarations: [
-        ExplorerComponent,
-    ],
+  declarations: [
+    ExplorerComponent,
+  ],
 
-    imports: [
-        CommonModule,
-        AngularMaterialModule,
-        ReactiveFormsModule,
-        FormsModule
-    ],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
 
-    exports: [ExplorerComponent],
-    
+  exports: [ExplorerComponent],
+
 })
-export class ExplorerModule { }
+export class ExplorerModule {
+}
