dbd1990eec634bf139340e2e10089bce1752fc6f
zvone <zvonimir_relja@yahoo.com>
2017-11-11 11:43:08 +0100

    backend - GET /countries same as mock data

--- backend/apps/csyllabusapi/views/countries.py
+++ backend/apps/csyllabusapi/views/countries.py
@@ -1,6 +1,7 @@
 from rest_framework.parsers import JSONParser, FileUploadParser
 from rest_framework.views import APIView
 
+from ..models import City
 from ..models import Country
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -20,10 +21,23 @@ class CountryView(APIView):
     def get(self, request):
         countries = Country.objects.all()
         result = []
+
         for country in countries:
+            cities = City.objects.filter(country=country)
+            cityList = []
+            for city in cities:
+                one_city = {}
+                one_city['name'] = city.name
+                one_city['id'] = city.id
+                cityList.append(one_city)
             one_country = {}
+            one_country['id'] = country.id
             one_country['name'] = country.name
+            one_country['modified'] = country.modified
+            one_country['created'] = country.created
+            one_country['cities'] = cityList
             result.append(one_country)
+
 
         return Response(result)
 
