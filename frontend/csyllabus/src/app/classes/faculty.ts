0c3eb4d92957c24b099e5383e4df2dc3a1d19120
Emanuel Guberovic <emanuel.guberovic@fer.hr>
2017-11-13 20:17:33 +0100

    Variable bck explorer (#12)
    * added a router, fixed 10/13 unit test failures and fied bunch of lint errors
    
    * faculties autoselect instead of programs

--- frontend/csyllabus/src/app/classes/faculty.ts
+++ frontend/csyllabus/src/app/classes/faculty.ts
@@ -2,7 +2,7 @@ export class Faculty {
   id: Number;
   university_id: Number;
   city_id: Number;
-  name: String;
+  name: string;
 	created: String;
 	modified: String;
 	programs: Array<Object>;
