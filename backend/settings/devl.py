1c22b6cfe4364f364618847533821a6aedce35db
filt27 <filt.27@gmail.com>
2017-11-23 16:41:49 +0100

    devl.py

--- backend/settings/devl.py
+++ backend/settings/devl.py
@@ -1,6 +1,7 @@
 import os
 
 from .common import *
+from .db import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '{{ secret_key }}'
@@ -8,25 +9,16 @@ SECRET_KEY = '{{ secret_key }}'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'filip',
-        'PASSWORD': 'filipo',
-       # 'NAME': '{{ project_name }}',
-	    'NAME': 'csfer',
-    }
-}
 
 INTERNAL_IPS = ['192.168.56.1']
 
 INSTALLED_APPS += (
-    'autofixture',
-    'csyllabusapi',
-    'django.contrib.postgres',
-
+   'autofixture',
+   'csyllabusapi',
+   'django.contrib.postgres',
+   'corsheaders'
 )
 
 STATICFILES_DIRS.append(
-    os.path.join(BASE_DIR, os.pardir, 'frontend', 'build'),
+   os.path.join(BASE_DIR, os.pardir, 'frontend', 'build'),
 )
