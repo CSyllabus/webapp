d32284eb951e89d3c0e7972c028246ebb632bb1a
Emanuel Guberovic <emanuel.guberovic@fer.hr>
2017-11-23 22:13:27 +0100

    Update db.py

--- backend/settings/db.py
+++ backend/settings/db.py
@@ -1,10 +1,8 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'csyllabus',
-         'PASSWORD': 'Csyllabus1!',
-        'NAME': 'csyllabus',
-		'HOST': 'localhost'
-	#'NAME': 'csyllabusfer',
+        'USER': '',
+       # 'NAME': '{{ project_name }}',
+	    'NAME': 'csfer',
     }
 }
