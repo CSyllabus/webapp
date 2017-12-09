59db8d1e81a3a566b6bfaf61aa3a3453e8d4eb47
zvone <zvonimir_relja@yahoo.com>
2017-12-09 21:38:18 +0100

    nav bar responsive fix

--- frontend/csyllabus/src/app/components/navbar/navbar.component.ts
+++ frontend/csyllabus/src/app/components/navbar/navbar.component.ts
@@ -5,21 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
   constructor() { }
-  
-  ngOnInit() {
-     /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
-  }
-  myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
- 
-  
+
+
+
 }
