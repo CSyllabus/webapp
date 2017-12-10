354d830ab7607b4c9d9ca399ec2722ce85bf58af
zvone <zvonimir_relja@yahoo.com>
2017-12-10 21:27:59 +0100

    docs design start

--- frontend/csyllabus/src/app/components/navbar/navbar.component.ts
+++ frontend/csyllabus/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent  {
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
-
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( "#" + f )
+      if ( element ) element.scrollIntoView ( element )
+    });
+  }
 
 
 }
