84273d32136dc1caf4601d88192e827e5e1210d9
Adrien Roques <adrien.roques.31@outlook.fr>
2017-12-12 00:02:10 +0100

    Delete sh

--- config.sh
+++
@@ -1,7 +0,0 @@
-python manage.py migrate
-python manage.py loadtestdata users.EmailUser:100
-python manage.py loaddata backend/apps/csyllabusapi/fixtures/fer_fixtures_json.json
-python manage.py loaddata backend/apps/csyllabusapi/fixtures/laquila_fixtures_json.json
-python manage.py loaddata backend/apps/csyllabusapi/fixtures/mockup_fixtures_json.json
-python manage.py course_similarity
-python manage.py createsuperuser
