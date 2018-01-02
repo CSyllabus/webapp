581be9dd7c96a3074f4c113ed25fb09004634038
Sebastian Mayoral <obandomayoral@gmail.com>
2018-01-03 00:49:07 +0100

    Proba All the comments

--- frontend/csyllabus/src/app/components/social/social.component.ts
+++ frontend/csyllabus/src/app/components/social/social.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './../../services/courses.service';
+import { Comment } from './../../classes/comment';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from "angular4-social-login";
 import { FacebookLoginProvider, GoogleLoginProvider } from "angular4-social-login";
 import { SocialUser } from "angular4-social-login";
+
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.css']
 })
 export class SocialComponent implements OnInit {
+  private comments: any = [];
   private user: SocialUser;
   private loggedIn: boolean;
+  @Input() courseId: number;
 
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,private coursesService: CoursesService) {
+    this.comments=this.coursesService.getAllCommentsByCourse( this.courseId);
+   }
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
