084e4e091a31b7e78065b737add6abe74810b15c
filt27 <filt.27@gmail.com>
2018-01-11 18:58:18 +0100

    landing responsive

--- backend/settings/db.py
+++ backend/settings/db.py
@@ -1,9 +1,8 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-         'PASSWORD': 'Password1!',
-        'NAME': 'csyllabusfer',
-	#'NAME': 'csyllabusfer',
+        'USER': '',
+       # 'NAME': '{{ project_name }}',
+	    'NAME': 'csfer',
     }
 }
